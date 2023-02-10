112ec48d20f68ac3254ff9965a1d8f5fc11197c6
salsabilazahraandinaaa <andinasalsabillazahra@gmail.com>
2023-02-10 14:41:31 +0700

    commit get post list

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SiswaC;
+use App\Http\Controllers\PostC;
 
-Route:: get('/',function(){
-    dd ('Hello World !');
+route::get('/', function(){
+    return 'Hello World API !';
 });
-
-route::get('/siswa',[SiswaC::class,'index']);
-route::post('/siswa/store',[PerpustakaanController::class,'store']);
-route::get('/siswa/show{id}',[PerpustakaanController::class,'show']);
-route::post('/siswa/update/{id}', [PerpustakaanController::class ,'update']);
-route::get('/siswa/destroy/{id}', [PerpustakaanController::class ,'destroy']);
+route::get('/posts',[PostC::class,'index']);
